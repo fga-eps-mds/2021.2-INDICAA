355e75415a6ba9f14146152ef0cd3da12a45a040
gabrielm2q <200018167@aluno.unb.br>
2022-03-26 23:33:18 -0300

    :repeat:(#86): refatoração do código

--- src/mapeamento_materias.py
+++ src/mapeamento_materias.py
@@ -1,48 +1,55 @@
+# Importando as bibliotecas e módulos necessários para a execução do scraping
 import time
-import requests
-#import pandas as pd
-#from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
-#import json
 
-
+# Armazenando a URL da página inicial da listagem do SIGAA
 url = "https://sig.unb.br/sigaa/public/turmas/listar.jsf?aba=p-ensino"
 
+# Configurando o driver do navegador e passando a URL do SIGAA para ele
 option = Options()
 option.headless = True
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
+servicoFirefox = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(service=servicoFirefox, options=option)
 driver.get(url)
-time.sleep(6)
 
+# Aqui temos uma pausa de 2 segundos para que o navegador carregue as
+# informações necessárias antes de iniciar o scraping
+time.sleep(2)
 
-#driver.find_element_by_xpath("//*[@id='formTurma:inputDepto']").click()
-driver.find_element_by_id("formTurma:inputDepto").click()
-driver.find_element_by_xpath('//*[@id="formTurma:inputDepto"]/option[79]').click()
-driver.find_element_by_name('formTurma:j_id_jsp_1370969402_11').click()
+# Aqui estamos selecionando a opção "Graduação"
+driver.find_element(By.ID, "formTurma:inputNivel").click()
+driver.find_element(By.XPATH, '//*[@id="formTurma:inputNivel"]/option[3]').click()
 
- 
+# Aqui estamos selecionando o departamento "Faculdade do Gama"
+driver.find_element(By.ID, "formTurma:inputDepto").click()
+driver.find_element(By.XPATH, '//*[@id="formTurma:inputDepto"]/option[79]').click()
 
+# Clicando no botão de busca
+driver.find_element(By.NAME, 'formTurma:j_id_jsp_1370969402_11').click()
+
+# Aqui temos uma pausa de 2 segundos para que o navegador carregue as
+# informações necessárias antes de iniciar o scraping
+time.sleep(2)
+
+# contadorMaterias armazenará a quantidade de matérias contadas na lista
 contadorMaterias = 0
-element = driver.find_elements_by_class_name('tituloDisciplina')
+
+# Aqui estamos importando uma lista com todos os títulos de matérias
+# presentes na página do SIGAA
+element = driver.find_elements(By.CLASS_NAME, 'tituloDisciplina')
+
+# Vamos passar por todos os elementos (matérias) da lista de oferta
+# para printar todas elas e contar a quantidade total de matérias
 for k in element:
     html_content = k.get_attribute('innerHTML')
     print(html_content)
     contadorMaterias += 1
 
-print('Total de disciplinas apresentadas na listagem: ', contadorMaterias)
+print('Quantidade de matérias apresentadas na listagem: ', contadorMaterias)
 
-# element1 = driver.find_elements_by_class_name('linhaPar')
-# element2 = driver.find_elements_by_class_name('linhaImpar')
-# for k in element1:
-#     html_content = k.get_attribute('outerHTML')
-#     #print(html_content)
-#     i += 1
-# for k in element2:
-#     html_content = k.get_attribute('outerHTML')
-#     #print(html_content)
-#     i += 1
-
+# Encerrando o navegador
 driver.quit()
