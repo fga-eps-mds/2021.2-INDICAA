65945275b863ada15ed2dc96346e0eeb6b819e16
gabrielm2q <200018167@aluno.unb.br>
2022-03-27 23:27:08 -0300

    :repeat:(#86): refatoração do código
    Co-authored-by: oCaioOliveira <ocaio.ooliveira@gmail.com>

--- src/mapeamento_materias.py
+++ src/mapeamento_materias.py
@@ -1,55 +1,83 @@
 # Importando as bibliotecas e módulos necessários para a execução do scraping
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 
-# Armazenando a URL da página inicial da listagem do SIGAA
-url = "https://sig.unb.br/sigaa/public/turmas/listar.jsf?aba=p-ensino"
 
-# Configurando o driver do navegador e passando a URL do SIGAA para ele
+
+# Configurando o driver do navegador
 option = Options()
 option.headless = True
 servicoFirefox = Service(GeckoDriverManager().install())
 driver = webdriver.Firefox(service=servicoFirefox, options=option)
-driver.get(url)
 
-# Aqui temos uma pausa de 2 segundos para que o navegador carregue as
-# informações necessárias antes de iniciar o scraping
-time.sleep(2)
+
+
+# Passando a URL do SIGAA para o Navegador acessar
+def acessarURL():
+    # Armazenando a URL da página inicial da listagem do SIGAA
+    url = "https://sig.unb.br/sigaa/public/turmas/listar.jsf?aba=p-ensino"
+    driver.get(url)
 
 # Aqui estamos selecionando a opção "Graduação"
-driver.find_element(By.ID, "formTurma:inputNivel").click()
-driver.find_element(By.XPATH, '//*[@id="formTurma:inputNivel"]/option[3]').click()
+def selecionarNivelEnsino():
+    botaoCampoEnsino = driver.find_element(By.ID, "formTurma:inputNivel")
+    botaoGraduacao = driver.find_element(By.XPATH, '//*[@id="formTurma:inputNivel"]/option[3]')
+    botaoCampoEnsino.click()
+    botaoGraduacao.click()
 
 # Aqui estamos selecionando o departamento "Faculdade do Gama"
-driver.find_element(By.ID, "formTurma:inputDepto").click()
-driver.find_element(By.XPATH, '//*[@id="formTurma:inputDepto"]/option[79]').click()
+def selecionarUnidade():
+    botaoUnidade = driver.find_element(By.ID, "formTurma:inputDepto")
+    botaoFGA = driver.find_element(By.XPATH, '//*[@id="formTurma:inputDepto"]/option[79]')
+    botaoUnidade.click()
+    botaoFGA.click()
 
 # Clicando no botão de busca
-driver.find_element(By.NAME, 'formTurma:j_id_jsp_1370969402_11').click()
+def acionarBotaoBuscar():
+    botaoBuscar = driver.find_element(By.NAME, 'formTurma:j_id_jsp_1370969402_11')
+    botaoBuscar.click()
 
-# Aqui temos uma pausa de 2 segundos para que o navegador carregue as
-# informações necessárias antes de iniciar o scraping
-time.sleep(2)
 
-# contadorMaterias armazenará a quantidade de matérias contadas na lista
-contadorMaterias = 0
+def verificaVagas():
+    # contadorMaterias armazenará a quantidade de matérias contadas na lista
+    contadorMaterias = 0
 
-# Aqui estamos importando uma lista com todos os títulos de matérias
-# presentes na página do SIGAA
-element = driver.find_elements(By.CLASS_NAME, 'tituloDisciplina')
+    # Aqui estamos importando uma lista com todos os títulos de matérias
+    # presentes na página do SIGAA
+    listaMaterias = driver.find_elements(By.CLASS_NAME, 'tituloDisciplina')
 
-# Vamos passar por todos os elementos (matérias) da lista de oferta
-# para printar todas elas e contar a quantidade total de matérias
-for k in element:
-    html_content = k.get_attribute('innerHTML')
-    print(html_content)
-    contadorMaterias += 1
+    # Vamos passar por todos os elementos (matérias) da lista de oferta
+    # para printar todas elas e contar a quantidade total de matérias
+    for materia in listaMaterias:
+        nomeMateria = materia.get_attribute('innerHTML')
+        print(nomeMateria)
+        contadorMaterias += 1
+    
+    return contadorMaterias
 
-print('Quantidade de matérias apresentadas na listagem: ', contadorMaterias)
 
-# Encerrando o navegador
-driver.quit()
+def main():
+    acessarURL()
+
+    # Aqui temos uma pausa de 2 segundos para que o navegador carregue as
+    # informações necessárias antes de iniciar o scraping
+    driver.implicitly_wait(2)
+
+    selecionarNivelEnsino()
+    selecionarUnidade()
+    acionarBotaoBuscar()
+
+    # Aqui temos uma pausa de 2 segundos para que o navegador carregue as
+    # informações necessárias antes de iniciar o scraping
+    driver.implicitly_wait(2)
+
+    contMaterias = verificaVagas()
+    print('Quantidade de matérias apresentadas na listagem: ', contMaterias)
+
+    # Encerrando o navegador
+    driver.quit()
+
+main()
