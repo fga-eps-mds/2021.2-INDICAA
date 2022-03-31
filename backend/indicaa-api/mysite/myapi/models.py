c41a74283cb3b7bfa72b496a157ac12591d44d51
AdneMoretti <morettiadne1@gmail.com>
2022-03-31 18:27:23 -0300

    :repeat:(#89):pequenas alteracoes em models

--- backend/indicaa-api/mysite/myapi/models.py
+++ backend/indicaa-api/mysite/myapi/models.py
@@ -1,37 +1,63 @@
-from types import NoneType
+# from types import NoneType
 from django.db import models
 
 # Create your models here.
 
 
 class Departamento(models.Model):
-    nome = models.CharField(max_length=255, primary_key=True)
+    nome = models.CharField(
+        max_length=255, 
+        primary_key=True
+    )
 
     def __str__(self):
         return self.nome
 
 
 class Materia(models.Model):
-    codigoMateria = models.CharField(max_length=10, primary_key=True)
-    nome = models.CharField(max_length=255)
-    cargaHoraria = models.CharField(max_length=3) 
-    departamento = models.ForeignKey(Departamento, on_delete=models.RESTRICT)
+    codigoMateria = models.CharField(
+        max_length=10, 
+        primary_key=True
+    )
+    nome = models.CharField(
+        max_length=255
+    )
+    cargaHoraria = models.CharField(
+        max_length=3
+    ) 
+    departamento = models.ForeignKey(
+        Departamento, 
+        on_delete=models.RESTRICT
+    )
 
     def __str__(self):
         return self.nome
 
 
 class Turma(models.Model):
-    idTurma = models.AutoField(primary_key=True)
-    docente = models.CharField(max_length=255)
-    codigoTurma = models.CharField(max_length=2)
-    horario = models.CharField(max_length=20)
+    idTurma = models.AutoField(
+        primary_key=True
+    )
+    docente = models.CharField(
+        max_length=255
+    )
+    codigoTurma = models.CharField(
+        max_length=2
+    )
+    horario = models.CharField(
+        max_length=20
+    )
     vagasOfertadas = models.IntegerField()
     vagasOcupadas = models.IntegerField()
-    local = models.CharField(max_length=255)
+    local = models.CharField(
+        max_length=255
+    )
     semestre = models.IntegerField()
     ano = models.IntegerField()
-    materia = models.ForeignKey(Materia, on_delete=models.RESTRICT)
+    materia = models.ForeignKey(
+        Materia, 
+        on_delete=models.RESTRICT
+    )
 
     def __str__(self):
         return self.docente
--- backend/indicaa-api/mysite/myapi/serializers.py
+++ backend/indicaa-api/mysite/myapi/serializers.py
@@ -11,10 +11,25 @@ class DepartamentoSerializer(serializers.HyperlinkedModelSerializer):
 class MateriaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Materia
-        fields = ('nome', 'codigoMateria', 'cargaHoraria', 'departamento')
+        fields = (
+            'nome', 
+            'codigoMateria', 
+            'cargaHoraria', 
+            'departamento'
+        )
 
         
 class TurmaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Turma
-        fields = ('idTurma', 'docente', 'codigoTurma', 'horario', 'vagasOfertadas', 'vagasOcupadas', 'local', 'ano', 'semestre', 'materia')
+        fields = (
+            'idTurma', 
+            'docente', 
+            'codigoTurma', 
+            'horario', 
+            'vagasOfertadas', 
+            'vagasOcupadas', 
+            'local', 'ano', 
+            'semestre', 
+            'materia'
+        )
