da9abfcee53d445449491b2a11811c415b02166f
AdneMoretti <morettiadne1@gmail.com>
2022-04-13 16:33:55 -0300

    :repeat:(#118):alterando docker-compose.yml

--- src/indicaa-api/config/settings.py
+++ src/indicaa-api/config/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'HOST': os.environ.get('DB_HOST'),
-    'NAME': os.environ.get('DB_NAME'),
-    'USER': os.environ.get('DB_USER'),
-    'PASSWORD': os.environ.get('DB_PASS'),
+    'HOST': os.environ.get('POSTGRES_HOST'),
+    'NAME': os.environ.get('POSTGRES_NAME'),
+    'USER': os.environ.get('POSTGRES_USER'),
+    'PORT': os.environ.get('POSTGRES_PORT'),
+    'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
   }
 }
 
